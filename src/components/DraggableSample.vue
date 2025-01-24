d1fcdcd3fa396e898901e61fc376fbbae72ecfb3
Iftee97 <tawsifhaque97@gmail.com>
2025-01-24 16:49:12 +0600

    initial commit

---
+++ src/components/DraggableSample.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <VueDraggable
+      v-model="list"
+      animation="150"
+      ghostClass="ghost"
+      class="container"
+    >
+      <div v-for="item in list" :key="item.id" class="item">
+        {{ item.name }}
+      </div>
+    </VueDraggable>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+import { VueDraggable } from "vue-draggable-plus";
+
+export default defineComponent({
+  name: "DraggableSample",
+
+  components: {
+    VueDraggable,
+  },
+
+  setup() {
+    const list = ref([
+      {
+        name: "Joao",
+        id: 1,
+      },
+      {
+        name: "Jean",
+        id: 2,
+      },
+      {
+        name: "Johanna",
+        id: 3,
+      },
+      {
+        name: "Juan",
+        id: 4,
+      },
+    ]);
+
+    return { list };
+  },
+});
+</script>
+
+<style>
+.container {
+  display: grid;
+  gap: 10px;
+}
+.item {
+  width: 200px;
+  background-color: red;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  cursor: move;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50"
+  >
+    <div class="bg-white p-6 rounded shadow-lg">
+      <h2 class="text-lg font-bold mb-4">Enter number of people</h2>
+      <input
+        type="number"
+        v-model="count"
+        min="20"
+        max="100"
+        class="border border-gray-300 rounded p-2 w-full mb-4"
+      />
+      <div class="flex justify-end space-x-2">
+        <button
+          class="bg-blue-500 text-white px-4 py-2 rounded"
+          @click="handleStart"
+        >
+          Start
+        </button>
+        <button class="bg-gray-300 px-4 py-2 rounded" @click="onClose">
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Modal",
+
+  props: ["onClose", "onStart"],
+
+  setup(props) {
+    const count = ref(20);
+
+    const handleStart = () => {
+      if (count.value >= 20 && count.value <= 100) {
+        props.onStart(count.value);
+      } else {
+        alert("Please enter a number between 20 and 100.");
+      }
+    };
+
+    return { count, handleStart };
+  },
+});
+</script>
